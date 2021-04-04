fa92ff335374c813351775fe67cdf47075bdad61
loboda4450 <loboda4450@gmail.com>
2021-04-04 19:44:42 +0200

    added packet generator

--- Packet.py
+++ Packet.py
@@ -1,8 +1,9 @@
-from numpy.random import random_sample
+from numpy.random import random_sample, randint
 
 
 class Packet:
-	def __init__(self, _epsilon: float, _log: bool):
+	def __init__(self, _epsilon: float, _size: int, _log: bool):
 		self.epsilon: float = _epsilon  # prawdopodobieństwo wysłania pakietu
-		self.is_sent: bool = (round(random_sample(), 1) == _epsilon)  # czy pakiet zostanie wysłany
+		self.is_sent: bool = (random_sample() >= _epsilon)  # czy pakiet zostanie wysłany
+		self.packet_size: int = _size
 		self.log: bool = _log
--- User.py
+++ User.py
@@ -3,20 +3,30 @@ from typing import List
 from ResourceBlock import ResourceBlock
 from Packet import Packet
 
-import numpy.random as random
+from numpy.random import randint
 
 
 class User:
 	def __init__(self, _log: bool):
-		self.d: int = random.randint(low=1, high=10) * 250  # [b] losowa (rozkład jednostajny) liczba odbieranych danych przez użytkownika
-		self.user_rb_list: List[ResourceBlock] = list()  # lista przydzielonych bloków zasobów użytkownikowi
-		self.packet_list: List[Packet] = list()  # lista pakietów przypisanych do użytkownika
+		self.log: bool = _log
+		self.d: int = randint(low=1, high=10) * 250  # [b] losowa (rozkład jednostajny) liczba odbieranych danych przez użytkownika
+		self.user_rb_list: List[ResourceBlock] = [ResourceBlock(_log=self.log) for _ in range(3)]  # lista przydzielonych bloków zasobów użytkownikowi
+		self.packet_list: List[Packet] = self.generate_packets()  # lista pakietów przypisanych do użytkownika
 		self.prev_d: List[ResourceBlock] = list()  # lista poprzednio przydzielonych bloków zasobów (używana do obliczania sum_d)
 		self.sum_d: int = 0  # nie ma sensu z tego wyciągać średniej, jeżeli numa >= numb, to numa/5 >= numb/5
-		self.log: bool = _log
 
 		if self.log:
 			logging.getLogger(__name__).info(msg=f'Created user: {self.d, self.user_rb_list, self.sum_d}')
+
+	def generate_packets(self):
+		remaining = self.d
+		pckt_list = list()
+		while remaining > 0:
+			datasize = min(randint(self.d/2), remaining)
+			remaining -= datasize
+			pckt_list.append(Packet(_epsilon=0.1, _size=datasize, _log=self.log))
+
+		return pckt_list
 
 	def update_d(self) -> int:
 		return self.d
